a9128052b4230ce5c59b81b078044d1f0c37752b
phuong210 <thuphuongnguyen337@gmail.com>
2019-03-27 20:18:46 +0300

    them file main.cpp len git

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "std.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("create new file source!!! \n");
+	return 0;
+}
